49a633886202546043ce733368fda92fe07bc0f5
thuhuyen-2308 <22T1020163@husc.edu.vn>
2026-04-06 19:29:08 +0700

    ok

--- SV22T1020163/SV22T1020163.Admin/NewFolder/CryptHelper.cs
+++
@@ -1,6 +0,0 @@
-namespace SV22T1020163.Admin;
-
-public static class CryptHelper
-{
-    public static string HashMD5(string input) => BusinessLayers.PasswordHasher.HashMd5(input);
-}
--- SV22T1020163/SV22T1020163.Shop/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace SV22T1020163.Shop.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
